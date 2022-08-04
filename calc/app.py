39ddcc74db27dced8b2aec616bc61ceeda32388e
dqoborsi <dqoborsi@gmail.com>
2022-08-04 11:13:30 -0700

    assignment complete but getting 404 != 200 error when doing test

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,32 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route('/add')
+def a():
+  a = int(request.args['a'])
+  b = int(request.args['b'])
+
+  return f"{add(a, b)}"
+
+@app.route('/sub')
+def s():
+  a = int(request.args['a'])
+  b = int(request.args['b'])
+
+  return f"{sub(a, b)}"
+
+@app.route('/mult')
+def m():
+  a = int(request.args['a'])
+  b = int(request.args['b'])
+
+  return  f"{mult(a, b)}"
+
+@app.route('/div')
+def d():
+  a = int(request.args['a'])
+  b = int(request.args['b'])
+
+  return f"{div(a, b)}"
